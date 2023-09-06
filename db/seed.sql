4da6e129e310b19301c29bcaebca47c8c079a971
Brian Klein <bklein1981@gmail.com>
2023-09-06 12:17:31 -0500

    reseeded db

--- db/seed.sql
+++ db/seed.sql
@@ -2,7 +2,8 @@ INSERT INTO departments (department_name)
 VALUES  ('Engineering'),
         ('Finance'),
         ('Legal'),
-        ('Sales');
+        ('Sales'),
+        ('Maintenance');
 
 INSERT INTO roles (title, salary, department_id)
 VALUES  ('Sales Lead', 60000, 4),
@@ -12,7 +13,9 @@ VALUES  ('Sales Lead', 60000, 4),
         ('Accounting Manager',90000, 2),
         ('Accounts Payable', 50000, 2),
         ('Corporate Counsel', 250000, 3),
-        ('Paralegal', 75000, 3);
+        ('Paralegal', 75000, 3),
+        ('Head Custodian', 50000, 5),
+        ('Nigh Custodian', 40000, 5);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES  ('John', 'Delorean', 1, NULL),
@@ -23,5 +26,6 @@ VALUES  ('John', 'Delorean', 1, NULL),
         ('Jack', 'Kennedy', 7, NULL),
         ('Paul', 'McCartney', 8, 6),
         ('Serena', 'Williams', 5, NULL),
-        ('Thomas', 'Cruise', 6, 8);
+        ('Thomas', 'Cruise', 6, 8),
+        ('Bertram', 'Russel', 9, NULL);
 
